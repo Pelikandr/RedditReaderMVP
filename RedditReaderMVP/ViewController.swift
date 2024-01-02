ebe57159c63aa9cddabacc1c77ed4e64ac7314d3
Denys Zaiakin <denis.zayakin@trackimo.com>
2024-01-02 19:05:32 +0200

    Initial Commit

---
+++ RedditReaderMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditReaderMVP
+//
+//  Created by TrackimoM1Pro on 02.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
